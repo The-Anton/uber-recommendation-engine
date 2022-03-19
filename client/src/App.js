e590114c4cb73ac9cb0916909a874028df85c2b3
The-Anton <shekhar59324@gmail.com>
2022-03-19 11:46:35 +0530

    fix: logos

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 
-import NavBar from "./common/Navbar";
 import Home from "./pages/Home";
-import Events from "./pages/Events";
-import HacktoberFest from "./pages/HacktoberFest";
-import Solutions from "./pages/Solutions";
-import Videos from "./pages/Videos";
-import Programs from "./pages/Programs";
-import Team from "./pages/Team";
-import Contact from "./pages/Contact";
+
 import NotFound from "./pages/NotFound";
 import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -32,17 +25,9 @@ class App extends Component {
             <ThemeProvider theme={theme}>
 
             <React.Fragment>
-                <NavBar />
                 <main className="container">
                     <Switch>
                         <Route path="/" exact component={Home}/>
-                        <Route path="/events" exact component={Events}/>
-                        <Route path="/programs" exact component={Programs}/>
-                        <Route path="/solutions" exact component={Solutions}/>
-                        <Route path="/videos" exact component={Videos}/>
-                        <Route path="/coreTeam" exact component={Team}/>
-                        <Route path="/contact" exact component={Contact}/>
-                        <Route path="/hacktoberFest" exact component={HacktoberFest}/>
                         <Route path="/notFound" component={NotFound}/>
                         <Redirect to="/notFound"/>
                     </Switch>
