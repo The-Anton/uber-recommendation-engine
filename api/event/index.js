e836043f4fb458bf2de0dd581f76a5e8fe5551f4
The-Anton <shekhar59324@gmail.com>
2022-03-27 17:23:53 +0530

    chore: remove unecessary files

--- api/event/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express') 
-const eventController = require("./controller.js")
-const router = express.Router();
-
-router.get('/all',eventController.getAllEvents);
-
-module.exports = router;
